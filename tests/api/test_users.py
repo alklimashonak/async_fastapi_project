b1c2068f0ec26bc65c494db0afa4c583c547dfe0
alkli <alklimashonak@gmail.com>
2022-12-29 22:26:53 +0100

    fix

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -8,62 +8,68 @@ from tests.utils.user import TEST_USER_EMAIL, TEST_USER_PASSWORD
 pytestmark = pytest.mark.anyio
 
 
-async def test_get_current_user_api_authorized_returns_user(
-        async_client: AsyncClient,
-        test_user: UserDB,
-) -> None:
-    token = create_access_token(subject=str(test_user.email))
-    headers = {'Authorization': f'bearer {token}'}
+class TestGetCurrentUserAPI:
+    async def test_get_current_user_api_authorized_returns_user(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        token = create_access_token(subject=str(test_user.email))
+        headers = {'Authorization': f'bearer {token}'}
 
-    response = await async_client.get('/api/users/me/', headers=headers)
+        response = await async_client.get('/api/users/me/', headers=headers)
 
-    assert response.status_code == 200
-    assert response.json()['id'] == str(test_user.id)
-    assert response.json()['email'] == test_user.email
+        assert response.status_code == 200
+        assert response.json()['id'] == str(test_user.id)
+        assert response.json()['email'] == test_user.email
 
+    async def test_get_current_user_api_unauthorized_raise_401(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        response = await async_client.get('/api/users/me/')
 
-async def test_get_current_user_api_unauthorized_raise_401(
-        async_client: AsyncClient,
-        test_user: UserDB,
-) -> None:
-    response = await async_client.get('/api/users/me/')
-
-    assert response.status_code == 401
-
-
-async def test_register_api_returns_user_info_with_token(
-        async_client: AsyncClient
-) -> None:
-    email = 'user123@gmail.com'
-    password = '1234'
-    payload = {
-        'email': email,
-        'password': password,
-    }
-
-    response = await async_client.post('/api/auth/register/', json=payload)
-
-    assert response.status_code == 200
-    assert response.json()['user']['email'] == email
-    assert response.json()['access_token']
+        assert response.status_code == 401
 
 
-async def test_failed_login_raise_401(
-        async_client: AsyncClient,
-        test_user: UserDB,
-) -> None:
-    email = TEST_USER_EMAIL
-    password = TEST_USER_PASSWORD + 'qwe'
+class TestRegisterAPI:
+    async def test_register_api_returns_user_info_with_token(
+            self,
+            async_client: AsyncClient
+    ) -> None:
+        email = 'user123@gmail.com'
+        password = '1234'
+        payload = {
+            'email': email,
+            'password': password,
+        }
 
-    payload = {
-        'username': email,
-        'password': password,
-    }
+        response = await async_client.post('/api/auth/register/', json=payload)
 
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
+        assert response.status_code == 200
+        assert response.json()['user']['email'] == email
+        assert response.json()['access_token']
 
-    response = await async_client.post('/api/auth/login/', headers=headers, data=payload)
 
-    assert response.status_code == 401
+class TestLoginAPI:
+    async def test_failed_login_raise_401(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        email = TEST_USER_EMAIL
+        password = TEST_USER_PASSWORD + 'qwe'
+
+        payload = {
+            'username': email,
+            'password': password,
+        }
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        response = await async_client.post('/api/auth/login/', headers=headers, data=payload)
+
+        assert response.status_code == 401
--- tests/test_deps.py
+++ tests/test_deps.py
@@ -9,22 +9,24 @@ from app.schemas.user import UserDB
 pytestmark = pytest.mark.anyio
 
 
-async def test_get_current_user_returns_user_correct_token(
-        async_client: AsyncClient,
-        test_user: UserDB,
-) -> None:
-    token = create_access_token(subject=test_user.email)
+class TestGetCurrentUser:
+    async def test_get_current_user_returns_user_correct_token(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        token = create_access_token(subject=test_user.email)
 
-    user = await get_current_user(token=token)
+        user = await get_current_user(token=token)
 
-    assert user.id == test_user.id
+        assert user.id == test_user.id
 
+    async def test_get_current_user_raise_401_incorrect_token(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        token = create_access_token(subject='failed@email.com')
 
-async def test_get_current_user_raise_401_incorrect_token(
-        async_client: AsyncClient,
-        test_user: UserDB,
-) -> None:
-    token = create_access_token(subject='failed@email.com')
-
-    with pytest.raises(HTTPException):
-        await get_current_user(token=token)
+        with pytest.raises(HTTPException):
+            await get_current_user(token=token)
