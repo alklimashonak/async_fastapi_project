c40e2343c088d0124d91c1b93e1b12a375903b14
alkli <alklimashonak@gmail.com>
2023-01-03 17:51:28 +0100

    add update team API and tests

--- app/api/api_v1/routers/teams.py
+++ app/api/api_v1/routers/teams.py
@@ -1,8 +1,9 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
+from starlette.status import HTTP_403_FORBIDDEN
 
 from app.api.dependencies import get_current_user
 from app.crud import crud_team
-from app.schemas.team import TeamResponse, TeamCreate
+from app.schemas.team import TeamResponse, TeamCreate, TeamUpdate
 from app.schemas.user import UserDB
 
 router = APIRouter()
@@ -15,3 +16,18 @@ async def create_team(
 ) -> TeamResponse:
     team = await crud_team.create(payload=team_in, owner_id=current_user.id)
     return TeamResponse(team=team)
+
+
+@router.put('/{team_id}/', response_model=TeamResponse)
+async def update_team(
+        team_id: int,
+        team_in: TeamUpdate,
+        current_user: UserDB = Depends(get_current_user),
+) -> TeamResponse:
+    team_to_update = await crud_team.get_team_by_id(team_id=team_id)
+
+    if team_to_update.owner_id != current_user.id:
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail='User can update only his own teams')
+
+    new_team = await crud_team.update(team_id=team_id, payload=team_in)
+    return TeamResponse(team=new_team)
--- tests/api/test_teams.py
+++ tests/api/test_teams.py
@@ -1,9 +1,12 @@
 import pytest
 from httpx import AsyncClient
-from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK
+from pydantic import EmailStr, SecretStr
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK, HTTP_403_FORBIDDEN
 
 from app.core.security import create_access_token
-from app.schemas.user import UserDB
+from app.crud import crud_user
+from app.schemas.user import UserDB, UserCreate
+from tests.utils.team import create_test_team
 
 pytestmark = pytest.mark.anyio
 
@@ -41,3 +44,52 @@ class TestCreateTeamAPI:
         response = await async_client.post('/api/teams/', json=team_data)
 
         assert response.status_code == HTTP_401_UNAUTHORIZED
+
+
+class TestUpdateTeamAPI:
+    async def test_user_can_update_his_own_team(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        team = await create_test_team(owner_id=test_user.id)
+        token = create_access_token(subject=test_user.email)
+
+        payload = {
+            'name': 'Updated Team'
+        }
+
+        headers = {
+            'Authorization': f'bearer {token}'
+        }
+
+        response = await async_client.put(f'/api/teams/{team.id}/', json=payload, headers=headers)
+
+        assert response.status_code == HTTP_200_OK
+        assert response.json()['team']['name'] == payload.get('name')
+
+    async def test_user_cant_update_not_his_own_team(
+            self,
+            async_client: AsyncClient,
+            test_user: UserDB,
+    ) -> None:
+        user_in = UserCreate(
+            email=EmailStr('someuser@example.com'),
+            password=SecretStr('1234')
+        )
+
+        team_payload = {
+            'name': 'New Team Name'
+        }
+
+        user = await crud_user.create(payload=user_in)
+        team = await create_test_team(owner_id=test_user.id)
+        token = create_access_token(subject=user.email)
+
+        headers = {
+            'Authorization': f'bearer {token}'
+        }
+
+        response = await async_client.put(f'/api/teams/{team.id}/', json=team_payload, headers=headers)
+
+        assert response.status_code == HTTP_403_FORBIDDEN
