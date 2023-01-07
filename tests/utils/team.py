9cc3fb04ca02e61248a8b57191a1c1846aa4d58d
alkli <alklimashonak@gmail.com>
2023-01-07 16:40:34 +0100

    add team utils for tests

--- tests/utils/team.py
+++ tests/utils/team.py
@@ -5,6 +5,7 @@ from pydantic import UUID4
 
 from app.crud import crud_team
 from app.schemas.team import TeamDB, TeamCreate
+from tests.utils.driver import create_test_driver
 
 
 def get_random_string(length=8):
@@ -17,9 +18,12 @@ async def create_test_team(
         owner_id: UUID4,
         team_name: str | None = None,
 ) -> TeamDB:
+    drivers = [await create_test_driver() for _ in range(5)]
+    drivers_ids = {driver.id for driver in drivers}
+
     if team_name:
-        team_data = TeamCreate(name=team_name)
+        team_data = TeamCreate(name=team_name, picks=drivers_ids)
     else:
-        team_data = TeamCreate(name=get_random_string())
+        team_data = TeamCreate(name=get_random_string(), picks=drivers_ids)
 
     return await crud_team.create(team_in=team_data, owner_id=owner_id)
